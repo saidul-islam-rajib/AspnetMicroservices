883302836eafbdf26e2d1dda4506320a8dd7b21e
Rajib <saidul.bd@asa-international.com>
2023-07-05 22:08:42 +0600

    Add `ShoppingCart` and `ShoppingCartItem` entity

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public decimal ProductId { get; set; }
+        public decimal ProductName { get; set; }
+    }
+}
