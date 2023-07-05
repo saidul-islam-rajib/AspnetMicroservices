7bea003e50b9d0fb35df2c0a04a06a89727465a9
Rajib <saidul.bd@asa-international.com>
2023-07-05 22:36:52 +0600

    Add `Basket` interface

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    /// <summary>
+    /// Basket interface
+    /// </summary>
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
