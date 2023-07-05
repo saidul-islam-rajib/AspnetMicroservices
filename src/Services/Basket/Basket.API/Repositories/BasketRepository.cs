abff987de2bf03e73665380bf51a67b55f473b06
Rajib <saidul.bd@asa-international.com>
2023-07-05 22:54:46 +0600

    Add repository pattern implementation for `Basket` microservices

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,46 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    /// <summary>
+    /// Concrete Basket repository class
+    /// </summary>
+    public class BasketRepository : IBasketRepository
+    {
+        // We are using IDistributed cache object as redis cache
+        private readonly IDistributedCache _redisCache;
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+        
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if(String.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+        
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            // `SetStringAsync` will perform add/edit/delete operation based on key value
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName); // will search for updated basket data by username and will return found value else will return null
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+               
+
+        
+    }
+}
